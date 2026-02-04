c3b3d005554d41acdf71bc56e6a4e72b9ce607fc
tomatosugarpickled <tjdgh1851@gmail.com>
2026-02-04 15:41:37 +0900

    table 정정입니다

--- src/main/resources/sql/team_feeds.sql
+++ src/main/resources/sql/team_feeds.sql
@@ -4,7 +4,7 @@ create table tbl_team_feeds
     id                         bigint unsigned auto_increment primary key,
     team_feed_profile          varchar(255) unique not null,
     team_technology_stack      varchar(255) unique not null,
-    team_Detailed_introduction varchar(65535)      not null,
+    team_Detailed_introduction varchar(65535)      not null
 #     team_size
 #     team_current_level
 #     team_recruit_position
