f50495f48b0d5d2c1ecc4b679caf6ec063a61721
tomatosugarpickled <tjdgh1851@gmail.com>
2026-02-19 10:48:18 +0900

    page에 member정보 뿌리기

---
+++ src/main/java/com/app/candm/mapper/MypageMapper.java
@@ -0,0 +1,10 @@
+package com.app.candm.mapper;
+
+import com.app.candm.dto.mypage.MemberCareerDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MypageMapper {
+    public void careerInsert(MemberCareerDTO memberCareerDTO);
+
+}
